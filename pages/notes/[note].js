e5606f059a2fe751b20bc4d6ab2328e347422448
kriscfoster <kfoster@salesforce.com>
2020-06-23 23:22:34 +0100

    part 2 materialui

--- pages/notes/[note].js
+++ pages/notes/[note].js
@@ -1,6 +1,19 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { Container, Typography, TextField, Button } from '@material-ui/core';
 
 export default function Note() {
   const router = useRouter();
-  return <h1>this is a note {router.query.note}</h1>
+  return <Container>
+    <Link href="/">
+      <Button
+        variant="contained"
+        color="primary"
+      >
+        Back
+      </Button>
+    </Link>
+    <Typography variant="h2">{router.query.note}</Typography>
+    <TextField multiline fullWidth value={"This is where my note text should go"}/>
+  </Container>
 }
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Button, Box, Container, Paper, Typography, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const notes = [
   { name: 'my-note' },
@@ -6,15 +8,38 @@ const notes = [
   { name: 'another-note' },
 ];
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    marginBottom: theme.spacing(3),
+  },
+  paper: {
+    padding: theme.spacing(3),
+    margin: theme.spacing(1),
+    color: 'white',
+    background: theme.palette.primary.main,
+  }
+}));
+
 export default function Note() {
-  return <div>
-    <h1>Notes Page</h1>
+  const classes = useStyles();
+
+  return <Container>
+    <div className={classes.root}>
     { notes.map((note) => {
-      return <div>
-        <Link as={"/notes/" + note.name} href="/notes/[note]">
-          <a>{note.name}</a>
-        </Link>
-      </div>
+      return <Link as={"/notes/" + note.name} href="/notes/[note]">
+        <Paper className={classes.paper}>
+          <Typography>{note.name}</Typography>
+        </Paper>
+      </Link>
     })}
-  </div>
+    </div>
+    <Typography variant="h5">Create Note</Typography>
+    <form>
+      <TextField id="standard-basic" label="Name" />
+      <Box pt={3}>
+        <Button color="secondary" variant="contained">Create Note</Button>
+      </Box>
+    </form>
+  </Container>
 }
