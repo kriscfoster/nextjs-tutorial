9abc8aaea60ea6f9b4af242c10bdac82208843ae
Kris Foster <krisfoster@live.ie>
2020-06-25 06:57:28 +0100

    material ui

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,4 +1,12 @@
 import Link from 'next/link';
+import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Button from "@material-ui/core/Button";
 
 const notes = [
   { name: 'my-note' },
@@ -8,13 +16,28 @@ const notes = [
 
 export default function Note() {
   return <div>
-    <h1>Notes Page</h1>
-    { notes.map((note, i) => {
-      return <div key={i}>
-        <Link as={"/notes/" + note.name} href="/notes/[note]">
-          <a>{note.name}</a>
-        </Link>
-      </div>
-    })}
+    <Typography variant="h2">Notes Page</Typography>
+    <TableContainer>
+      <Table>
+        <TableBody>
+          { notes.map((note, i) => {
+            return (
+              <TableRow key={i}>
+                <TableCell>
+                  <Link as={"/notes/" + note.name} href="/notes/[note]">
+                    <a>{note.name}</a>
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Button variant="contained" color="secondary">
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 }
