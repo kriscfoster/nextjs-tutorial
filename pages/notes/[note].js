b19e5f414d02a301eea9f4f89d018bab9d55d88e
Kris Foster <krisfoster@live.ie>
2020-06-25 06:15:58 +0100

    part 2

--- pages/notes/[note].js
+++ pages/notes/[note].js
@@ -1,19 +1,13 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { Container, Typography, TextField, Button } from '@material-ui/core';
+import { Container, Typography, TextField, Button, Box } from '@material-ui/core';
 
 export default function Note() {
   const router = useRouter();
   return <Container>
-    <Link href="/">
-      <Button
-        variant="contained"
-        color="primary"
-      >
-        Back
-      </Button>
-    </Link>
-    <Typography variant="h2">{router.query.note}</Typography>
-    <TextField multiline fullWidth value={"This is where my note text should go"}/>
+    <Box mt={2}>
+      <Typography variant="h2">{router.query.note}</Typography>
+      <TextField multiline fullWidth value={"This is where my note text should go"}/>
+    </Box>
   </Container>
 }
--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,6 +1,17 @@
 import Link from 'next/link';
-import { Button, Box, Container, Paper, Typography, TextField } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  Button,
+  Box,
+  Container,
+  Paper,
+  Typography,
+  TextField,
+  TableContainer,
+  Table,
+  TableRow,
+  TableCell,
+  TableBody
+} from '@material-ui/core';
 
 const notes = [
   { name: 'my-note' },
@@ -8,38 +19,36 @@ const notes = [
   { name: 'another-note' },
 ];
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    marginBottom: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(3),
-    margin: theme.spacing(1),
-    color: 'white',
-    background: theme.palette.primary.main,
-  }
-}));
-
 export default function Note() {
-  const classes = useStyles();
-
   return <Container>
-    <div className={classes.root}>
-    { notes.map((note, i) => {
-      return <Link key={i} as={"/notes/" + note.name} href="/notes/[note]">
-        <Paper className={classes.paper}>
-          <Typography>{note.name}</Typography>
-        </Paper>
-      </Link>
-    })}
-    </div>
-    <Typography variant="h5">Create Note</Typography>
-    <form>
-      <TextField id="standard-basic" label="Name" />
-      <Box pt={3}>
-        <Button color="secondary" variant="contained">Create Note</Button>
-      </Box>
-    </form>
+    <Box mt={2}>
+      <Typography variant="h2">my notes</Typography>
+      <TableContainer>
+        <Table>
+          <TableBody>
+            { notes.map((note, i) => {
+              return <TableRow key={i}>
+                <TableCell>
+                  <Link key={i} as={"/notes/" + note.name} href="/notes/[note]">
+                    {note.name}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <Button color="secondary" variant="contained">Delete</Button>
+                </TableCell>
+              </TableRow>
+            })}
+            <TableRow>
+              <TableCell>
+                <TextField id="standard-basic" label="Name" />
+              </TableCell>
+              <TableCell>
+                <Button color="primary" variant="contained">Create Note</Button>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   </Container>
 }
